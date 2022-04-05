f76440cd5a381bd7cd717d44840bc34d65ce934d
dev3style <hungpt58.uet@gmail.com>
2022-04-05 20:55:22 +0700

    remove unused code

--- src/main/java/com/uet/spring/practice/config/WebSecurityConfig.java
+++ src/main/java/com/uet/spring/practice/config/WebSecurityConfig.java
@@ -3,24 +3,13 @@ package com.uet.spring.practice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @EnableWebMvc
 @Configuration
@@ -30,40 +19,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthManager authManager;
 
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        var user = User.builder()
-//                .username("hungpham")
-//                .password("12345")
-//                .passwordEncoder(p -> p) // raw -> encrypted. Need to declare a PasswordEncoder Bean
-//                .roles("USER")
-//                .authorities("USER")
-//                .build();
-//
-//        var admin =  User.builder()
-//                .username("anboo33")
-//                .password("12345")
-//                .passwordEncoder(p -> p) // raw -> encrypted. Need to declare a PasswordEncoder Bean
-//                .roles("ADMIN")
-//                .authorities("ADMIN")
-//                .build();
-//        // return authManager; //Custom
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
+    @Bean
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("anboo33")
+    protected UserDetailsService userDetailsService() {
+        var user = User.builder()
+                .username("hungpham")
                 .password("12345")
+                .passwordEncoder(p -> p) // raw -> encrypted. Need to declare a PasswordEncoder Bean
+                .roles("USER")
+                .authorities("USER")
+                .build();
+
+        var admin =  User.builder()
+                .username("anboo33")
+                .password("12345")
+                .passwordEncoder(p -> p) // raw -> encrypted. Need to declare a PasswordEncoder Bean
                 .roles("ADMIN")
                 .authorities("ADMIN")
-                .and()
-                .withUser("hungpham")
-                .password("12345")
-                .authorities("USER")
-                .roles("USER");
+                .build();
+        // return authManager; //Custom
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Override
